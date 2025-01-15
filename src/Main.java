dfd0e2b3840c3b63fa03ce7480659d57acc7b875
Madumarov Davron <menaceteacher@gmail.com>
2025-01-16 01:31:27 +0500

    add

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,8 @@ public class Main {
         public static String encode(String text) {
             return URLEncoder.encode(text, StandardCharsets.UTF_8);
         }
+        public static String decode(String encodedText) {
+            return URLDecoder.decode(encodedText, StandardCharsets.UTF_8);
+        }
     }
 }
